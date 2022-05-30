9aea090dc316cb27381d62d09539ae95e309c301
Thiago Carneiro <thiagocarneiro1998@gmail.com>
2022-05-29 21:06:58 -0300

    adicao bootstrap

--- index.js
+++ index.js
@@ -6,25 +6,9 @@ app.set("view engine","ejs")
 // arquivos estaticos
 app.use(express.static('public'))
 // rota inicial
-app.get("/:nome/:lang",(req, res) => {
-    var nome = req.params.nome
-    var lang = req.params.lang
-    var exibirMsg = false
-    
-    var produtos = [
-        {nome: "Gillete", preco: 5.50},
-        {nome: "Bonzano", preco: 12.2},
-        {nome: "maça", preco: 0.70}
-    ]
+app.get("/",(req, res) => {
     // renderisando index.ejs e passando variaveis para o html
-    res.render("index.ejs",{
-        nome: nome,
-        lang: lang,
-        empresa: "ThPerguntas",
-        nascimento: 1998,
-        msg: exibirMsg,
-        produtos: produtos
-    })
+    res.render("index.ejs")
 })
 
 // abertura servidor
