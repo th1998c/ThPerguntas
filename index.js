a333e20670a24d6d92189bb3f29eb1883995c317
thiago carneiro <thiagocarneiro1998@gmail.com>
2022-06-03 00:49:57 -0300

    recebendo dados do form no node

--- index.js
+++ index.js
@@ -2,23 +2,34 @@ const express = require("express")
 const req = require("express/lib/request")
 const app = express()
 
+//chamando body-parser
+const bodyParser = require("body-parser")
+
 // chamano ejs
 app.set("view engine","ejs")
 // arquivos estaticos
 app.use(express.static('public'))
+//body-parser
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+//rotas
+
 // rota inicial
 app.get("/",(req, res) => {
     // renderisando index.ejs e passando variaveis para o html
     res.render("index.ejs")
 })
-
+// rota de página do formulario de pergunta
 app.get("/perguntar",(req,res) =>{
     res.render("perguntar.ejs")
 })
 
 //rota de submit de pergunta
 app.post("/enviarpergunta",(req,res) =>{
-    res.send('formulário recebido')
+    var titulo = req.body.titulo
+    var descricao = req.body.descricao
+    res.send('formulário recebido! título: '+titulo+'<br> descrição: '+descricao)
 })
 
 // abertura servidor
